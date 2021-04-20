8fd314ad64a6dd00a3649f4b16f6eb3688058b2b
RobotCherries <zdip14@gmail.com>
2021-04-20 10:39:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ultimatecourses-angular-pro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ultimatecourses-angular-pro)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AuthFormModule } from "./auth-form/auth-form.module";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AuthFormModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
