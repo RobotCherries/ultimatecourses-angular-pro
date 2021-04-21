a24fe15bfafbaba15b31d09e66561f50296f99ea
RobotCherries <zdip14@gmail.com>
2021-04-21 10:39:25 +0300

    1 file modified

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -5,39 +5,57 @@ import {
   ContentChild,
   AfterContentInit,
   ContentChildren,
-  QueryList
+  QueryList,
+  ViewChild,
+  AfterViewInit
 } from "@angular/core";
 
 import { User } from "./auth-form.interface";
+import { AuthMessageComponent } from "./auth-message/auth-message.component";
 import { AuthRememberComponent } from "./auth-remember/auth-remember.component";
 
 @Component({
   selector: "auth-form",
   templateUrl: "auth-form.component.html"
 })
-export class AuthFormComponent implements AfterContentInit {
+export class AuthFormComponent implements AfterViewInit, AfterContentInit {
+  // Inputs & Outputs
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
-  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
-  // @ContentChildren(AuthRememberComponent)
-  // remembers: QueryList<AuthRememberComponent>;
+  // View & Content Children
+  @ViewChild(AuthMessageComponent) authMessage: AuthMessageComponent;
+  // @ViewChildren(AuthMessageComponent) authMessages: QueryList<AuthMessageComponent>;
 
+  @ContentChild(AuthRememberComponent) authRemember: AuthRememberComponent;
+  // @ContentChildren(AuthRememberComponent) authRemembers: QueryList<AuthRememberComponent>;
+
+  // Booleans
   showAuthRememberMessage: boolean = false;
 
   constructor() {}
 
+  ngAfterViewInit(): void {
+    console.log(this.authMessage);
+  }
+
   ngAfterContentInit(): void {
-    this.showFormMessages();
+    this.initFormMessages();
   }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
   }
 
-  showFormMessages(): void {
-    if (this.remember) {
+  initFormMessages(): void {
+    // View Children
+    if (this.authMessage?.loggedInDays) {
+      this.authMessage.loggedInDays = 30;
+    }
+
+    // Content Children
+    if (this.authRemember) {
       // Subscribe to the RememberMe component
-      this.remember.check.subscribe(
+      this.authRemember.check.subscribe(
         (isChecked: boolean) => (this.showAuthRememberMessage = isChecked)
       );
 
